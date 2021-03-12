fa1e83ceed195e3720c88373bf18513266de92b3
Alexander Mami <lalik77@inbox.ru>
2021-03-12 23:46:03 +0300

    In idea  project structure->modules  + web , add server Tomcat , was a lot of problem , inspiration from alishev and google. Add SprinConfig and MySpringDispatcherServlets from alishev

---
+++ src/main/java/com/mami/luv2codes/controller/SpringConfig.java
@@ -0,0 +1,54 @@
+package com.mami.luv2codes.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+@ComponentScan("com.mami.luv2codes")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver bean = new SpringResourceTemplateResolver();
+        bean.setApplicationContext(applicationContext);
+        bean.setPrefix("/WEB-INF/lib/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+
+
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp().prefix("/WEB-INF/views/");
+    }
+
+
+}
+
+
---
+++ web/WEB-INF/views/main-menu.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Spring demo</title>
+
+
+</head>
+<body>
+
+<h2>Hello from Spring MVC</h2>
+
+</body>
+</html>
