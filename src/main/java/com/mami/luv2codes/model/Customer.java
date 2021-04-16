da6eb63b135987fb006ce902599564edd72cc51a
Alexander Mami <algru81@gmail.com>
2021-04-16 15:02:32 +0300

    Valiadte number range , validate pattern wit regular expression , handle String input for integer fields and loading custom messages ressource

--- src/main/java/com/mami/luv2codes/model/Customer.java
+++ src/main/java/com/mami/luv2codes/model/Customer.java
@@ -1,8 +1,7 @@
 package com.mami.luv2codes.model;
 
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
 
@@ -12,6 +11,30 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @NotNull(message = "is requiered")
+    @Min(value = 0 , message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}" , message = "only 5 chrs/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getLastName() {
         return lastName;
