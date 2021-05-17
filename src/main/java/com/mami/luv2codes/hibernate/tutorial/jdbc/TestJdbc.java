5ae1a103163fba30232a47c1a2755a4132205eaa
Alexander Mami <algru81@gmail.com>
2021-05-17 17:33:37 +0300

    Initial commit , Test connection , Change TestJdbc

--- src/main/java/com/mami/luv2codes/hibernate/tutorial/jdbc/TestJdbc.java
+++ src/main/java/com/mami/luv2codes/hibernate/tutorial/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 public class TestJdbc {
 
     public static void main(String[] args) {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-04-one-to-many-uni?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
         String user = "hbstudent";
         String pass = "hbstudent";
 
