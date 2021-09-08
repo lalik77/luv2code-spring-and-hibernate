18442c712060d8f6c0dcb44401165b72149f51b6
Alexander Mami <algru81@gmail.com>
2021-09-08 09:54:08 +0300

    lesson4 - Match with any  return type . finish lesson 4

--- src/main/java/com/mami/luv2codes/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/mami/luv2codes/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyDemoLoggingAspect {
 
-    @Before("execution( void add*())")
+    @Before("execution( * add*())")
     public void addAccountAdvice() {
 
         System.out.println("\n =====>>> Execution @Before advice on method addAccount");
