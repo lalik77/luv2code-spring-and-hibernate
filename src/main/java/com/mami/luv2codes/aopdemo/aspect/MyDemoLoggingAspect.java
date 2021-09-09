d3b7e4a73eca1626959ce79874e73e021dd61cea
Alexander Mami <algru81@gmail.com>
2021-09-09 18:36:40 +0300

    Match method with Any Parameters

--- src/main/java/com/mami/luv2codes/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/mami/luv2codes/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyDemoLoggingAspect {
 
-    @Before("execution( * add*(com.mami.luv2codes.aopdemo.Account,..))")
+    @Before("execution( * add*(..))")
     public void addAccountAdvice() {
 
         System.out.println("\n =====>>> Execution @Before advice on method addAccount");
