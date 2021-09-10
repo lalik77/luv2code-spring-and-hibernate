56b32591c90f399b30b663b5fc9ffa02effa2782
Alexander Mami <algru81@gmail.com>
2021-09-10 12:03:28 +0300

    Add point cut declaration

--- src/main/java/com/mami/luv2codes/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/mami/luv2codes/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,13 +2,20 @@ package com.mami.luv2codes.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class MyDemoLoggingAspect {
 
-    @Before("execution( * com.mami.luv2codes.aopdemo.dao.*.* (..))")
+
+    @Pointcut("execution( * com.mami.luv2codes.aopdemo.dao.*.* (..))")
+    private void forDaoPackage() {
+
+    }
+
+    @Before("forDaoPackage()")
     public void addAccountAdvice() {
 
         System.out.println("\n =====>>> Execution @Before advice on method addAccount");
